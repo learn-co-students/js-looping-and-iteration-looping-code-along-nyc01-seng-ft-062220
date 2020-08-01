2932c023644669a5021eda7e7ac4bc56fbb71064
axiumone <axiumone@gmail.com>
2020-08-01 19:52:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const names = [ 'Lisa', 'Kaitlin', 'Jan' ];
+
+function writeCards(names) {
+    let thankYou = []
+    let i = 0;
+    while (i < names.length) {
+      thankYou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      i++;
+    }
+   return thankYou
+  }
+   
+  writeCards(names);
+
+function countDown(num) {
+    for (let i = 0; i <= num; num--) {
+        console.log(num);
+    };
+}
+
+countDown(num);
