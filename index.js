d8f8c55ce152f4e38be25784d18c1e5c81afeccd
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-08-01 18:15:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+
+function writeCards(namesArray, event) {
+    const cardsArray = []
+    for (let i = 0; i < namesArray.length; i++) {
+        cardsArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardsArray;
+
+}
+
+function countDown(int){
+    while (int >= 0) {
+        console.log(int)
+        int--    
+    }
+}
