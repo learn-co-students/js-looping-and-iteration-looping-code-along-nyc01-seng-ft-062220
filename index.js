b518830a0caf32f20b09d36d9d63c16c8897b7ec
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-08-01 14:01:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+function writeCards(array, event) {
+
+    let thank_you_array = []
+
+    for (let i = 0; i < array.length; i++) {
+        thank_you_array.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thank_you_array
+}
+
+
+function countDown(number) {
+    for (let i = number + 1; i === 0, i--;) {
+        console.log(i)
+    }
+}
