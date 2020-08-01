de7fd6ce64c1412cd4e807b05e0909e5e5076eeb
hahm-d <dhahm01@gmail.com>
2020-07-31 22:59:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+    return array.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+
+function countDown(num){
+    while (num >= 0) {
+       console.log(num);
+       num--;
+    }
+}
+
+console.log(countDown(3))
